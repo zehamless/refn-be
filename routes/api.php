ba8ff85db0f7f1e890ce80f9191b016789e70272
Mahez_ <batubattery25@gmail.com>
2024-12-31 14:54:44 +0700

    ✨ feat(auth): Implement authentication features
    🔥 chore(frontend): Remove frontend scaffolding
    
    ✨ feat(tests): Add authentication tests
    
    ♻️ refactor(app): Update bootstrap and configuration files

--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,18 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
-    Route::resource('orders', OrderController::class)->names('orders');
-    Route::resource('services', ServiceController::class);
-    Route::resource('users', UserController::class);
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('dashboard/recent-orders', [DashboardController::class, 'getRecentOrder'])->name('dashboard.recent-orders');
-    Route::get('dashboard/processing-orders', [DashboardController::class, 'getProcessingOrder'])->name('dashboard.processing-orders');
+    Route::get('/check', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+    Route::middleware('auth')->group(function () {
+        Route::resource('orders', OrderController::class)->names('orders');
+        Route::resource('services', ServiceController::class);
+        Route::resource('users', UserController::class);
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('dashboard/recent-orders', [DashboardController::class, 'getRecentOrder'])->name('dashboard.recent-orders');
+        Route::get('dashboard/processing-orders', [DashboardController::class, 'getProcessingOrder'])->name('dashboard.processing-orders');
+    });
+    require __DIR__ . '/auth.php';
 });
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,22 +1,7 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get(route('dashboard'));
-    dump($response->content());
+test('example', function () {
+    $response = $this->get('/');
+
     $response->assertStatus(200);
-    $response->assertJsonStructure([
-        'unpaid',
-        'processing',
-        'completed',
-        'cancelled',
-    ]);
-});
-it('get recent order', function () {
-    $response = \Pest\Laravel\get(route('dashboard.recent-orders'));
-    dump($response->content());
-});
-it('get processing order', function () {
-    $response = \Pest\Laravel\get(route('dashboard.processing-orders'));
-//    dump($response->content());
-    assert($response->status() === 200);
 });
